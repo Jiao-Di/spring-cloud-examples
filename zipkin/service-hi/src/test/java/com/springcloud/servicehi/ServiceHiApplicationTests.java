756ec547e84a3cfda8c71c05886487c2df874ed6
jd <jd@jddeMacBook-Pro.local>
2020-08-02 22:40:12 +0800

    zipkin and config

---
+++ zipkin/service-hi/src/test/java/com/springcloud/servicehi/ServiceHiApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.springcloud.servicehi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
